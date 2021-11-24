3900c8d24e4737781fd881887e46c46291077941
hanungss <hanungsoekamti@gmail.com>
2021-11-24 11:21:19 +0700

    add controller

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/landing_page', function () {
+    return 'Welcome to home page';
 });
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/home','HomeController@index');
